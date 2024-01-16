e6e0de1c58d077d20d13265aa77450db51618386
techmagus <techmagus@im.youronly.one>
2024-01-17 02:34:51 +0800

    upd: deps

--- assets/js/twemoji-settings.js
+++ assets/js/twemoji-settings.js
@@ -1,4 +1,11 @@
+/* See:
+      https://github.com/jdecked/twemoji
+      https://cdn.jsdelivr.net/gh/jdecked/twemoji@latest/assets/
+      https://cdn.jsdelivr.net/npm/@twemoji/api@latest/dist/twemoji.min.js
+*/
+
 /* Source: https://stackoverflow.com/a/40310653/306674 */
+
 function onReady(insertTwemoji) {
   var readyStateCheckInterval = setInterval(function() {
     if (document && document.readyState === 'complete') {
@@ -9,7 +16,7 @@ function onReady(insertTwemoji) {
 }
 onReady(function() {
   twemoji.parse(document.body, {
-    base: 'https://cdn.jsdelivr.net/gh/jdecked/twemoji/assets/',
+    base: 'https://cdn.jsdelivr.net/gh/jdecked/twemoji@latest/assets/',
     ext: '.png',
     folder: '72x72',
     callback: function(icon, options) {
