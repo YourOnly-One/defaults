00a82b0128afa57c86c13a711c3e22f730c90aff
techmagus <techmagus@im.youronly.one>
2024-06-05 17:36:03 +0800

    clean up and update

--- assets/js/disqus.js
+++ assets/js/disqus.js
@@ -1,13 +1,12 @@
 import * as params from '@params';
 
-window.disqus_config = function () {
-  this.page.identifier = params.disqus_identifier_fm || params.disqus_identifier_auto;
-  this.page.title = params.disqus_title_fm || params.disqus_title_auto;
-  this.page.url = params.disqus_url_fm || params.disqus_url_auto;
+var disqus_config = function () {
+  this.page.identifier = params.disqus_identifier_manual || params.disqus_identifier_auto;
+  this.page.url = params.disqus_url_manual || params.disqus_url_auto;
+  this.page.title = params.disqus_title_manual || params.disqus_title_auto;
 };
 (function() {
   var d = document, s = d.createElement('script');
-  s.async = true;
   s.src = 'https://' + params.DisqusShortName + '.disqus.com/embed.js';
   s.setAttribute('data-timestamp', +new Date());
   (d.head || d.body).appendChild(s);
