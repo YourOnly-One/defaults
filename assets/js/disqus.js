8b155e1ed217978520d83c7a8a46508681ebbbb8
techmagus <techmagus@im.youronly.one>
2024-06-06 20:57:47 +0800

    ref: resources.ExecuteAsTemplate so we can pipe to safeJSStr; upd: cleaned up

--- assets/js/disqus.js
+++ assets/js/disqus.js
@@ -1,13 +1,16 @@
-import * as params from '@params';
-
-window.disqus_config = function () {
-  this.page.identifier = params.disqus_identifier;
-  this.page.url = params.disqus_url;
-  this.page.title = params.disqus_title;
+{{- $relcanonical := .Params.relcanonical }}
+{{- $relcanonical_path := (urls.Parse $relcanonical).Path }}
+{{- $disqus_identifier := or .Params.disqus_identifier (path.Base $relcanonical_path) }}
+{{- $disqus_url := or .Params.disqus.url $relcanonical }}
+{{- $disqus_title := (or .Params.disqus_title .Title) | htmlUnescape }}
+var disqus_config = function () {
+  this.page.identifier = `{{ $disqus_identifier }}`; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+  this.page.url = `{{ $disqus_url | html  }}`;  // Replace PAGE_URL with your page's canonical URL variable
+  this.page.title = `{{ $disqus_title | safeJSStr }}`;
 };
 (function() {
-  var d = document, s = d.createElement('script');
-  s.src = 'https://' + params.disqus_shortname + '.disqus.com/embed.js';
-  s.setAttribute('data-timestamp', +new Date());
+  var d = document, s = d.createElement(`script`);
+  s.src = `https://` + `{{ .Site.Config.Services.Disqus.Shortname }}` + `.disqus.com/embed.js`;
+  s.setAttribute(`data-timestamp`, +new Date());
   (d.head || d.body).appendChild(s);
 })();
